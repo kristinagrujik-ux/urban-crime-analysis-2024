ce739deb9924aa8d9e8f3e1be18f6820f5f1ef4d
kristina grujic <kristinagrujik@gmail.com>
2026-08-15 16:21:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,7 @@ elif "Организиран" in selected_sheet:
             val_24 = pd.to_numeric(row.iloc[6], errors='coerce')
             val_23 = pd.to_numeric(row.iloc[8], errors='coerce')
             
-            # Додаваме празно место на почетокот за да се оддалечи од линијата
-            categories.append("    " + cat)
+            categories.append(cat)
             vals_2024.append(val_24 if pd.notna(val_24) else 0)
             vals_2023.append(val_23 if pd.notna(val_23) else 0)
 
@@ -150,7 +149,7 @@ elif "Организиран" in selected_sheet:
     }).melt('Категорија', var_name='Година', value_name='Број')
 
     bars = alt.Chart(df_okg).mark_bar().encode(
-        y=alt.Y('Категорија:N', title='Категорија', sort=None, axis=alt.Axis(labelLimit=400, labelPadding=10)),
+        y=alt.Y('Категорија:N', title='Категорија', sort=None, axis=alt.Axis(labelLimit=450, labelPadding=15)),
         x=alt.X('Број:Q', title='Број на случаи', scale=alt.Scale(domain=[0, 10]), axis=alt.Axis(format='d')),
         color=alt.Color('Година:N', scale=alt.Scale(domain=['2024', '2023'], range=['#1f77b4', '#aec7e8'])),
         yOffset='Година:N',
